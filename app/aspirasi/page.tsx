a956feef4126ce1a4f5b53905bed1fc5dde48401
FaizZzhue <fynyourbaeid18@gmail.com>
2026-02-13 21:52:39 +0700

    refactor : pages

--- app/aspirasi/page.tsx
+++ app/aspirasi/page.tsx
@@ -1,4 +1,3 @@
-
 import Navbar from "@/components/common/Navbar";
 import FormAspirasiSection from "@/components/features/aspirasi/Form-Aspirasi-Section";
 import Footer from "@/components/common/Footer";
